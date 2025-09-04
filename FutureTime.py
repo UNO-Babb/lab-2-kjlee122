2f88be378d1b71013301ab0d15b476029b7de40e
Kjlee122 <147563359+kjlee122@users.noreply.github.com>
2025-09-04 01:05:49 +0000

    Finished

--- FutureTime.py
+++ FutureTime.py
@@ -9,16 +9,28 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = now.hour + 7
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
+  
 
   #TODO:
   #Ask user for hours
+  wantHour = input("What hour would you like to count down until? ")
+  wantHour = int(wantHour)
+  
+
   #Ask user for minutes
+  wantMinute = input("What minute would you like to count down until? ")
+  wantMinute = int(wantMinute)
 
   #Calculate the time after the user-supplied time has passed.
+  moreMins = wantMinute
+  moreHours = wantHour
+  futureMins = (currentMinute + moreMins) % 60
+  extraHour = (currentMinute + moreMins) // 60
+  futureHour = (currentHour + moreHours + extraHour )
+  print (futureHour,":",futureMins) 
 
   #Do not use any if statements in calculating the time.
 
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,14 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  question = input("Ask a question: ")
   #Answer question randomly with one of the options from your earlier list.
-
+  answers = ["As I see it, yes.","Ask again later.", "Better not tell you now.", "Cannot predict now." "Concentrate and ask again.", "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes – definitely.", "You may rely on it."]
+  r = random.random() * 20 
+  r = int(r)
+  response = answers[r]
+  print(response)
+ 
 
 if __name__ == '__main__':
   main()
